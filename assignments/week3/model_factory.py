b105c0be474906f07faa6827f182002c5cac76d9
zongxinc <zongxinc@bu.edu>
2023-02-06 12:23:44 -0500

    test

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,5 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 32, output_dim, 1, torch.nn.ReLU, torch.nn.init.ones_)
+    print("ReLU6 xavier_uniform_ 32 3")
+    return MLP(input_dim, 32, output_dim, 3, torch.nn.ReLU6, torch.nn.init.xavier_uniform_)
