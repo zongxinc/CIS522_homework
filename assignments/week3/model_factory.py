0c0773af79a441af801927c0fd8fd5086f96e567
zongxinc <zongxinc@bu.edu>
2023-02-06 12:33:25 -0500

    style change

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,6 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 32, output_dim, 3, torch.nn.ReLU6, torch.nn.init.xavier_uniform_)
+    return MLP(
+        input_dim, 32, output_dim, 3, torch.nn.ReLU6, torch.nn.init.xavier_uniform_
+    )
