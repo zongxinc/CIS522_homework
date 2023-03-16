b9ffe9cc4cdeea45b2c52f148b9913a6859b50cf
zongxinc <zongxinc@bu.edu>
2023-03-15 22:23:16 -0400

    test

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -7,7 +7,6 @@ class Model(torch.nn.Module):
     """
     Convolutional Neural Network
     """
-
     def __init__(self, num_channels: int, num_classes: int) -> None:
         """
         Initialize parameters of ConvNet
@@ -20,20 +19,10 @@ class Model(torch.nn.Module):
             None
         """
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu1 = nn.ReLU()
-
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU()
-
+        self.conv1 = nn.Conv2d(num_channels, 8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.fc1 = nn.Linear(64 * 16 * 16, 64)
-        self.bn3 = nn.BatchNorm1d(64)
-        self.relu3 = nn.ReLU()
-
+        self.fc1 = nn.Linear(16 * 16 * 16, 64)
         self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -48,19 +37,10 @@ class Model(torch.nn.Module):
           x: torch.tensor
             Output after passing x through Conv2d layer
         """
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
-
-        x = self.pool(x)
-        x = x.view(-1, 64 * 16 * 16)
-        x = self.fc1(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
-
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        # print(x.shape)
+        x = x.view(-1, 16 * 16 * 16)
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
